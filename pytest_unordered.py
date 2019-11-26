f95abf0bfa4594b37a306a8921ccc0caf009abab
utapyngo <ut@pyngo.tom.ru>
2019-11-26 14:40:14 +0700

    Format with black

--- pytest_unordered.py
+++ pytest_unordered.py
@@ -7,7 +7,10 @@ class Unordered:
     def __init__(self, expected):
         if not isinstance(expected, Sized):
             raise TypeError(
-                'cannot make unordered comparisons to non-sized values: {!r}'.format(expected))
+                "cannot make unordered comparisons to non-sized values: {!r}".format(
+                    expected
+                )
+            )
         self.expected = expected
 
     def __eq__(self, actual):
@@ -19,7 +22,7 @@ class Unordered:
         return not extra_left and not extra_right
 
     def __repr__(self):
-        return 'unordered({!r})'.format(self.expected)
+        return "unordered({!r})".format(self.expected)
 
     def __len__(self):
         return len(self.expected)
@@ -51,14 +54,14 @@ def pytest_assertrepr_compare(op, left, right):
             right = right.expected
         left_repr = saferepr(left)
         right_repr = saferepr(right)
-        result = ['{} {} {}'.format(left_repr, op, right_repr)]
+        result = ["{} {} {}".format(left_repr, op, right_repr)]
         extra_left, extra_right = _compare_eq_unordered(left, right)
         if extra_left:
-            result.append('Extra items in the left sequence:')
+            result.append("Extra items in the left sequence:")
             for item in extra_left:
                 result.append(saferepr(item))
         if extra_right:
-            result.append('Extra items in the right sequence:')
+            result.append("Extra items in the right sequence:")
             for item in extra_right:
                 result.append(saferepr(item))
         return result
--- tests/test_unordered.py
+++ tests/test_unordered.py
@@ -5,41 +5,39 @@ from pytest_unordered import _compare_eq_unordered
 from pytest_unordered import unordered
 
 
-@pytest.mark.parametrize('left,right', [
-    (unordered([1, 2, 3]), [3, 2, 1]),
-    ([3, 2, 1], unordered([1, 2, 3])),
-    (unordered([1, 2, {'a': unordered([4, 5, 6])}]), [{'a': [6, 5, 4]}, 2, 1]),
-    ([3, 2, {1: ['a', 'b']}], unordered([{1: ['a', 'b']}, 2, 3])),
-])
+@pytest.mark.parametrize(
+    "left,right",
+    [
+        (unordered([1, 2, 3]), [3, 2, 1]),
+        ([3, 2, 1], unordered([1, 2, 3])),
+        (unordered([1, 2, {"a": unordered([4, 5, 6])}]), [{"a": [6, 5, 4]}, 2, 1]),
+        ([3, 2, {1: ["a", "b"]}], unordered([{1: ["a", "b"]}, 2, 3])),
+    ],
+)
 def test_unordered(left, right):
     assert left == right
 
 
-@pytest.mark.parametrize('value', [
-    None,
-    type,
-    TypeError,
-])
+@pytest.mark.parametrize("value", [None, type, TypeError,])
 def test_unordered_non_sized_expected(value):
     with raises(TypeError):
         unordered(value)
 
 
-@pytest.mark.parametrize('value', [
-    None,
-    type,
-    TypeError,
-])
+@pytest.mark.parametrize("value", [None, type, TypeError,])
 def test_unordered_non_sized_actual(value):
     assert unordered([]) != value
 
 
-@pytest.mark.parametrize('left,right,extra_left,extra_right', [
-    ([1, 2, 3], [1, 2, 3, 4, 5], [], [4, 5]),
-    ([3, 2, 1], [1, 2, 3, 4, 5], [], [4, 5]),
-    ([3, 2, {1: ['a', 'b']}], [{1: ['a', 'b']}, 2, 3, 4, 5], [], [4, 5]),
-    ([3, 2, {1: ['a', 'b']}], [{1: unordered(['b', 'a'])}, 2, 3, 4, 5], [], [4, 5]),
-])
+@pytest.mark.parametrize(
+    "left,right,extra_left,extra_right",
+    [
+        ([1, 2, 3], [1, 2, 3, 4, 5], [], [4, 5]),
+        ([3, 2, 1], [1, 2, 3, 4, 5], [], [4, 5]),
+        ([3, 2, {1: ["a", "b"]}], [{1: ["a", "b"]}, 2, 3, 4, 5], [], [4, 5]),
+        ([3, 2, {1: ["a", "b"]}], [{1: unordered(["b", "a"])}, 2, 3, 4, 5], [], [4, 5]),
+    ],
+)
 def test_compare_eq_unordered(left, right, extra_left, extra_right):
     assert _compare_eq_unordered(left, right) == (extra_left, extra_right)
 
